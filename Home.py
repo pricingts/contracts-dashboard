c0b60df137c02d27a2c6335071939485407a7c6d
Trading Solutions <tradingsolutions@MacBook-Air-de-Trading.local>
2025-04-10 09:41:30 -0500

    modifications

--- Home.py
+++ Home.py
@@ -5,36 +5,6 @@ from collections import defaultdict
 
 st.set_page_config(page_title="Contracts Management", layout="wide")
 
-# def identity_role(email):
-    # commercial = [
-    #     "sales2@tradingsolutions.com", "sales1@tradingsolutions.com", "sales3@tradingsolutions.com",
-    #     "sales4@tradingsolutions.com", "sales5@tradingsolutions.com", "sales6@tradingsolutions.com",
-
-    #     "sales2@tradingsol.com", "sales1@tradingsol.com", "sales3@tradingsol.com",
-    #     "sales4@tradingsol.com", "sales5@tradingsol.com", "sales6@tradingsol.com",
-    # ]
-    # pricing = [
-    #     "pricing2@tradingsolutions.com", "pricing8@tradingsolutions.com", "pricing6@tradingsolutions.com", "pricing10@tradingsolutions.com", "pricing11@tradingsolutions.com", "customer9@tradingsolutions.com",
-    #     "pricing2@tradingsol.com", "pricing8@tradingsol.com", "pricing6@tradingsol.com", "pricing10@tradingsol.com", "pricing11@tradingsol.com", "customer9@tradingsol.com",
-    # ]
-    # admin = [
-    #     "manager@tradingsolutions.com", "jsanchez@tradingsolutions.com", "pricing2@tradingsolutions.com", "pricing@tradingsolutions.com", 
-    #     "manager@tradingsol.com", "jsanchez@tradingsol.com", "pricing2@tradingsol.com", "pricing@tradingsol.com",
-    # ]
-    # scrap_team = ["bds@tradingsolutions.com", "insidesales@tradingsolutions.com", "sales@tradingsolutions.com", "pricing3@tradingsolutions.com",
-    #                 "bds@tradingsol.com", "insidesales@tradingsol.com", "sales@tradingsol.com", "pricing3@tradingsol.com"]
-
-    # if email in commercial:
-    #     return "commercial"
-    # elif email in pricing:
-    #     return "pricing"
-    # elif email in admin:
-    #     return "admin"
-    # elif email in scrap_team:
-    #     return "scrap_team"
-    # else:
-    #     return None
-
 def identity_role(email):
     role_mapping = defaultdict(list)
 
@@ -43,17 +13,16 @@ def identity_role(email):
             "sales2", "sales1", "sales3", "sales4", "sales5", "sales6"
         ],
         "pricing": [
-            "pricing2", "pricing8", "pricing6", "pricing10", "pricing11", "customer9"
+            "pricing2", "pricing8", "pricing10", "pricing11", "customer9"
         ],
         "admin": [
             "manager", "jsanchez", "pricing2", "pricing"
         ],
         "scrap_team": [
-            "bds", "insidesales", "sales", "pricing3"
+            "bds", "insidesales", "sales", "pricing3", "pricing6"
         ]
     }
 
-    # Crear un set con todos los emails posibles para cada rol
     domain_variants = ["@tradingsolutions.com", "@tradingsol.com"]
 
     email_to_role = {}
